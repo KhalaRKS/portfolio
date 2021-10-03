44a585849019e190fa0a96640ec1a78957e6b240
KhalaRKS <albanokainer@hotmail.com>
2021-10-03 06:46:37 -0300

    nuevo script y agregado de animaciones

---
+++ script.js
@@ -0,0 +1,92 @@
+'use strict'
+ScrollReveal({ reset: false });
+/* HEADER */
+ScrollReveal().reveal('.contenedor-header',
+{
+    delay: 200,
+    duration: 800,
+    cleanup: true,
+})
+ScrollReveal().reveal('.contenedor-textos-header',
+{
+    delay: 350,
+    duration: 800
+})
+
+
+/*MAIN */
+ScrollReveal().reveal('.contenedor-article-aboutme',
+{
+    delay: 200,
+    duration: 800
+})
+
+ScrollReveal().reveal('.contenedor-informacion-aboutme',
+{
+    delay: 200,
+    duration: 1000
+})
+ScrollReveal().reveal('.contenedor-texto-aboutme',
+{
+    delay: 300,
+    duration: 1000
+})
+ScrollReveal().reveal('.contenedor-redes-aboutme',
+{
+    delay: 200,
+    duration: 800
+})
+ScrollReveal().reveal('.contenedor-botones-aboutme',
+{
+    delay: 200,
+    duration: 1000
+})
+ScrollReveal().reveal('.titulo-iconos',
+{
+    delay: 200,
+    duration: 1000
+})
+ScrollReveal().reveal('.iconos',
+{
+    delay: 200,
+    duration: 800
+})
+ScrollReveal().reveal('.contenedor-section-projects',
+{
+    delay: 200,
+    duration: 1000
+})
+ScrollReveal().reveal('.contenedor-proyectos',
+{
+    delay: 300,
+    duration: 800
+})
+ScrollReveal().reveal('.proyectos',
+{
+    delay: 200,
+    duration: 800
+})
+
+/* FOOTER */
+ScrollReveal().reveal('.contenedor-section-contact',
+{
+    delay: 200,
+    duration: 1000
+})
+ScrollReveal().reveal('.contenedor-formulario',
+{
+    delay: 250,
+    duration: 1000
+})
+ScrollReveal().reveal('.contenedor-redes',
+{
+    delay: 100,
+    duration: 1000
+})
+ScrollReveal().reveal('.iconos-redes',
+{
+    delay: 100,
+    duration: 1000
+})
+
+
